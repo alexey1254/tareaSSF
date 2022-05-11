7fd1347a054e9a98aa944f76c5a81339e0a43927
alexey1254 <alxkill54@gmail.com>
2022-05-11 16:01:06 +0100

    Se guarda mostrarDatos

--- mostrar_datos.sh
+++ mostrar_datos.sh
@@ -2,11 +2,19 @@
 
 arg1=$1
 arg2=$2
-
 fecha=`date`
 carga=`uptime`
 
-echo "Argumento1 $arg1"
-echo "Argumento2 $arg2"
-echo "La fecha es: $fecha"
-echo "La carga del sistema es de: $carga"
+echo "
+Argumento1 $arg1
+Argumento2 $arg2
+La fecha es: $fecha
+La carga del sistema es de: $carga" > $HOME/Escritorio/salida.txt
+
+echo "
+Argumento1 $arg1
+Argumento2 $arg2
+La fecha es: $fecha
+La carga del sistema es de: $carga
+
+Se ha guardado todo en el fichero ubicado en $HOME/Escritorio/salida.txt"
